521f97b6ce50eb4e8baa1ea082dd1ba83de04d56
Tyler <1251424057@qq.com>
2018-10-18 14:01:32 +0800

    asd

--- CSAPP/2.65.cpp
+++ CSAPP/2.65.cpp
@@ -1,13 +1,17 @@
-int odd_ones(unsigned x)
+//google popcount
+int odd_ones(unsigned x) //using while
 {
-    /*size_t count = 0;
-    ......
-    return count & 1;*/
     int ans = 0;
     while (x)
     {
         ++ans;
         x &= x - 1;
     }
-    return ans&1;
+    return ans & 1;
+}
+
+int hakmem_popcount(unsigned int n)
+{
+    unsigned tmp = n - ((n >> 1) & 033333333333) - ((n >> 2) & 011111111111);
+    return ((tmp + (tmp >> 3)) & 030707070707) & 1;
 }
