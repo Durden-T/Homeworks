b020d5bfb68522002763c25f0903404657e57417
Tyler <1251424057@qq.com>
2018-10-19 14:47:43 +0800

    asd

--- CSAPP/2.65.cpp
+++ CSAPP/2.65.cpp
@@ -11,10 +11,10 @@ int odd_ones(unsigned x) //using while
 
 int odd_ones(unsigned x)
 {
-    x ^= x >> 16; //x右移16位，然后将x的高16位和低16位异或，忽略32位数结果里面不相干的高16位。
+    x ^= x >> 16; 
     x ^= x >> 8;
     x ^= x >> 4;
     x ^= x >> 2;
     x ^= x >> 1;
-    return x&1;
+    return x & 1;
 }
